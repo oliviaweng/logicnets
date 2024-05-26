bbafd0098256c3427c8dc937f77f3e07b57388ef
Olivia Weng <oliviajweng@gmail.com>
2024-05-26 15:07:53 -0700

    update launch script

--- examples/hgcal_autoencoder/scripts/launch_ensemble_learning.sh
+++ examples/hgcal_autoencoder/scripts/launch_ensemble_learning.sh
@@ -34,7 +34,8 @@
 
 # small polylut modell
 # tmux new-session -d -s s00 "./scripts/run_gen_ensemble_exp.sh 0 ./ensemble_configs/averaging/small_polylut_seed3_configs/config1 ./averaging"
-tmux new-session -d -s s01 "./scripts/run_gen_ensemble_exp.sh 1 ./ensemble_configs/averaging/small_polylut_seed3_configs/config2 ./averaging"
+# tmux new-session -d -s s01 "./scripts/run_gen_ensemble_exp.sh 1 ./ensemble_configs/averaging/small_polylut_seed3_configs/config2 ./averaging"
+tmux new-session -d -s s02 "./scripts/run_gen_ensemble_exp.sh 0 ./ensemble_configs/averaging/small_polylut_seed3_configs/config3 ./averaging"
 
 # tmux new-session -d -s f00 "./scripts/run_gen_ensemble_exp.sh 0 ./ensemble_configs/averaging/large_fixed_mask_configs/config3 ./averaging"
 # tmux new-session -d -s f01 "./scripts/run_gen_ensemble_exp.sh 1 ./ensemble_configs/averaging/medium_fixed_mask_configs/config3 ./averaging"
