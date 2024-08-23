eca8c3e6f95183f395516e539fe71849bb6d4066
Olivia Weng <oliviajweng@gmail.com>
2024-08-23 12:12:37 -0700

    update neq2lut bash script w more commandline options

--- examples/hgcal_autoencoder/scripts/neq2lut.sh
+++ examples/hgcal_autoencoder/scripts/neq2lut.sh
@@ -16,21 +16,21 @@
 
 DATA_DIR=./data/hgcal22data_signal_driven_ttbar_v11/nElinks_5/
 DATA_FILE=./data/hgcal22data_signal_driven_ttbar_v11/hgcal22data_signal_driven_ttbar_v11_nELinks5.npy 
-SAVE_DIR=./hparams_config_test
-EXP_NAME=pipecleaner
-CONFIG=pipecleaner.yml
-CKPT="./hparams_config_test/pipecleaner/best_loss.pth"
+SAVE_DIR=./autoencoder_synth
+EXP_NAME=small_3ns
+CONFIG=./pareto_configs/small_pareto_model/small_pareto_model.yml
+CKPT=./pareto_configs/small_pareto_model/small_pareto_model_best_loss.pth
 
 
-CUDA_VISIBLE_DEVICES="" python3 neq2lut.py \
+CUDA_VISIBLE_DEVICES=0 python neq2lut.py \
    --data_dir $DATA_DIR \
    --data_file $DATA_FILE \
    --save_dir $SAVE_DIR \
    --experiment_name $EXP_NAME \
    --checkpoint $CKPT \
    --hparams_config $CONFIG \
-   --simulate_pre_synthesis_verilog \
-   --simulate_post_synthesis_verilog \
-   --compute_emd \
-   # --add_registers \
+   --add_registers \
+   --clock_period 3
+   # --simulate_pre_synthesis_verilog \
+   # --compute_emd \
    # --gpu \
