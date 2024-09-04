69c45b39c81ecc777d2259bc706faf3806c29666
Olivia Weng <oweng@gcloud>
2024-09-04 00:36:17 +0000

    fix gpu bug in models.py. add more mnist adaboost results

--- examples/jet_substructure/scripts/launch_ensembling_exp.sh
+++ examples/jet_substructure/scripts/launch_ensembling_exp.sh
@@ -2,6 +2,13 @@
 
 # Averaging 
 tmux new-session -d -s pj00 "./scripts/run_train_exp.sh 0 ./ensemble_configs/averaging/jscm_lite_deg2_configs/config1 ./averaging"
+tmux new-session -d -s pj01 "./scripts/run_train_exp.sh 0 ./ensemble_configs/averaging/jscm_lite_deg2_configs/config2 ./averaging"
+tmux new-session -d -s pj02 "./scripts/run_train_exp.sh 0 ./ensemble_configs/averaging/jscm_lite_deg2_configs/config3 ./averaging"
+
+tmux new-session -d -s pj03 "./scripts/run_train_exp.sh 1 ./ensemble_configs/averaging/jsc_xl_deg2_configs/config1 ./averaging"
+tmux new-session -d -s pj04 "./scripts/run_train_exp.sh 1 ./ensemble_configs/averaging/jsc_xl_deg2_configs/config2 ./averaging"
+tmux new-session -d -s pj05 "./scripts/run_train_exp.sh 1 ./ensemble_configs/averaging/jsc_xl_deg2_configs/config3 ./averaging"
+
 
 # Bagging
 # tmux new-session -d -s bag "./scripts/run_train_exp.sh 0 ./ensemble_configs/bagging/deg4_configs ./bagging"
