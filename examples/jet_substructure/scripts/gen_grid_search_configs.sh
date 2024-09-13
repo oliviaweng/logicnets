760f3570a8c2f953e8691c51b4e7fd072c635dff
Olivia Weng <oliviajweng@gmail.com>
2024-09-13 16:55:14 -0700

    prepare to launch shared input output layer averaging runs for jsc m and l

--- examples/jet_substructure/scripts/gen_grid_search_configs.sh
+++ examples/jet_substructure/scripts/gen_grid_search_configs.sh
@@ -58,7 +58,9 @@
 # python3 grid_search.py -c search_configs/jsc_m_ensemble_grid_search.yml -d ./ensemble_configs/averaging/medium_shared_sparse_input_layer --experiment_prefix medium_shared_sparse_input_layer
 # python3 grid_search.py -c search_configs/jsc_l_ensemble_grid_search.yml -d ./ensemble_configs/averaging/large_shared_sparse_input_layer --experiment_prefix large_shared_sparse_input_layer
 
-python3 grid_search.py -c search_configs/jsc_s_ensemble_grid_search.yml -d ./ensemble_configs/averaging/small_shared_sparse_output_layer --experiment_prefix small_shared_sparse_output_layer
+# python3 grid_search.py -c search_configs/jsc_s_ensemble_grid_search.yml -d ./ensemble_configs/averaging/small_shared_input_output_layers --experiment_prefix small_shared_input_output_layers
+# python3 grid_search.py -c search_configs/jsc_m_ensemble_grid_search.yml -d ./ensemble_configs/averaging/medium_shared_input_output_layers --experiment_prefix medium_shared_input_output_layers
+python3 grid_search.py -c search_configs/jsc_l_ensemble_grid_search.yml -d ./ensemble_configs/averaging/large_shared_input_output_layers --experiment_prefix medium_shared_input_output_layers
 
 # Bagging
 # python3 grid_search.py -c search_configs/jsc_s_ensemble_grid_search.yml -d ./ensemble_configs/bagging/small_independent_configs --experiment_prefix bagging_small_independent
