1d6a1089deb4c9f85a391453621e3b0b9a5f3e0d
Olivia Weng <oliviajweng@gmail.com>
2024-09-17 16:47:53 -0700

    WIP refactored mnist

---
+++ examples/mnist/scripts/launch_ensemble_learning.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# single models
+tmux new-session -d -s mxs "./scripts/run_gen_ensemble_exp.sh 0 ./logicnet_configs/mnist_xs ./minst_single_models/"
+tmux new-session -d -s ms "./scripts/run_gen_ensemble_exp.sh 0 ./logicnet_configs/mnist_s ./minst_single_models/"
+tmux new-session -d -s mm "./scripts/run_gen_ensemble_exp.sh 0 ./logicnet_configs/mnist_m ./minst_single_models/"
+
